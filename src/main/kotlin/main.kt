0a764bfa657995d34a4d41ab590625505f415f5b
Maksim Shcherbakov <i@max87.ru>
2024-03-10 20:17:03 +0300

    Autotest added

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,19 @@
 import kotlin.math.max
 
+val dayLimit: Int = -1
+val monthLimit: Int = -2
+val wrongTypeCard: Int = -3
 fun main() {
     println("Введите сумму перевода: ")
     println("Размер комиссии: ${transfer()}")
 }
-fun limitCheck(amount: Int, sumMonth: Int): Boolean {
+fun limitCheck(amount: Int, sumMonth: Int): Int {
     val limitDay = 150_000
     val limitMonth = 600_000
-    if (amount > limitDay) return false
-    if (amount + sumMonth > limitMonth) return false
-    return true
+    var result: Int = 0
+    if (amount > limitDay) result = dayLimit
+    if (amount + sumMonth > limitMonth) result = monthLimit
+    return result
 }
 
 fun visaMir(amount: Int): Int {
@@ -26,20 +30,21 @@ fun mastercardMaestrd(amount: Int): Int {
 fun vkPay(amount: Int, sumMonth: Int): Int {
     val atOnce = 15000
     val month = 40000
-    return if (amount <= atOnce && amount+sumMonth <= month) 0 else -2
+    return if (amount <= atOnce || amount+sumMonth <= month) 0 else -2
 }
 fun transfer(typeIn: String = "MIR", typeOut: String = "VKPay", sumMonth: Int = 0, amount: Int = readln().toInt()): Int {
-    if (!limitCheck(amount, sumMonth))
+    if (limitCheck(amount, sumMonth) == -1 || limitCheck(amount, sumMonth) == -2)
         println("Перевод невозможен, превышены лимиты!")
     var result: Int = 0
     if(typeOut == "VKPay") result = 0
-    else when(typeIn) {
-        "VKPay" -> result = vkPay(amount, sumMonth)
-        "MIR" -> result = visaMir(amount)
-        "VISA" -> result = visaMir(amount)
-        "MASTERCARD" -> result = mastercardMaestrd(amount)
-        "MAESTRO" -> result = mastercardMaestrd(amount)
-        else -> println("Неизвестный тип карты.")
+    else
+        when(typeIn) {
+            "VKPay" -> result = vkPay(amount, sumMonth)
+            "MIR" -> result = visaMir(amount)
+            "VISA" -> result = visaMir(amount)
+            "MASTERCARD" -> result = mastercardMaestrd(amount)
+            "MAESTRO" -> result = mastercardMaestrd(amount)
+            else -> result = wrongTypeCard
     }
     return result
 }
---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,193 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainKtTest {
+
+    @Test
+    fun visaMir() {
+        val comission = 0.0075
+        val minComission = 35
+        val amount = 50_000
+
+        val result = visaMir(amount)
+
+        assertEquals(375, result)
+    }
+
+    @Test
+    fun limitCheck1() {
+        val limitDay = 150_000
+        val limitMonth = 600_000
+        val amount = 50000
+        val sumMonth = 0
+        val result = limitCheck(amount, sumMonth)
+        assertEquals(0, result)
+    }
+    @Test
+    fun limitCheck2() {
+        val limitDay = 150_000
+        val limitMonth = 600_000
+        val amount = 250000
+        val sumMonth = 0
+        val result = limitCheck(amount, sumMonth)
+        assertEquals(-1, result)
+    }
+    @Test
+    fun limitCheck3() {
+        val limitDay = 150_000
+        val limitMonth = 600_000
+        val amount = 50000
+        val sumMonth = 350000
+        val result = limitCheck(amount, sumMonth)
+        assertEquals(0, result)
+    }
+    @Test
+    fun limitCheck4() {
+        val limitDay = 150_000
+        val limitMonth = 600_000
+        val amount = 150_000
+        val sumMonth = 550_000
+        val result = limitCheck(amount, sumMonth)
+        assertEquals(-2, result)
+    }
+
+    @Test
+    fun mastercardMaestrd1() {
+        val minMonthLimit = 300
+        val maxMonthLimit = 75_000
+        val comission = 0.006
+        val amount = 200
+        val result = mastercardMaestrd(amount)
+        assertEquals(21, result)
+    }
+    @Test
+    fun mastercardMaestrd2() {
+        val minMonthLimit = 300
+        val maxMonthLimit = 75_000
+        val comission = 0.006
+        val amount = 80000
+        val result = mastercardMaestrd(amount)
+        assertEquals(500, result)
+    }
+    @Test
+    fun mastercardMaestrd3() {
+        val minMonthLimit = 300
+        val maxMonthLimit = 75_000
+        val comission = 0.006
+        val amount = 50000
+        val result = mastercardMaestrd(amount)
+        assertEquals(0, result)
+    }
+
+
+    @Test
+    fun vkPay1() {
+        val atOnce = 15000
+        val month = 40000
+        val amount = 20_000
+        val sumMonth = 0
+        val result = vkPay(amount, sumMonth)
+        assertEquals(0, result)
+    }
+    @Test
+    fun vkPay2() {
+        val atOnce = 15000
+        val month = 40000
+        val amount = 20_000
+        val sumMonth = 25_000
+        val result = vkPay(amount, sumMonth)
+        assertEquals(-2, result)
+    }
+    @Test
+    fun vkPay3() {
+        val atOnce = 15000
+        val month = 40000
+        val amount = 50000
+        val sumMonth = 60000
+        val result = vkPay(amount, sumMonth)
+        assertEquals(-2, result)
+    }
+
+    @Test
+    fun transfer1() {
+        val typeIn = "MIR"
+        val typeOut = "VKPay"
+        val sumMonth = 0
+        val amount = 50000
+        val result = transfer(typeIn = typeIn, typeOut = typeOut, sumMonth = sumMonth, amount = amount)
+        assertEquals(0, result)
+    }
+    @Test
+    fun transfer2() {
+        val typeIn = "VKPay"
+        val typeOut = "VISA"
+        val sumMonth = 0
+        val amount = 50000
+        val result = transfer(typeIn = typeIn, typeOut = typeOut, sumMonth = sumMonth, amount = amount)
+        assertEquals(-2, result)
+    }
+    @Test
+    fun transfer3() {
+        val typeIn = "VISA"
+        val typeOut = "MIR"
+        val sumMonth = 0
+        val amount = 50000
+        val result = transfer(typeIn = typeIn, typeOut = typeOut, sumMonth = sumMonth, amount = amount)
+        assertEquals(375, result)
+    }
+    @Test
+    fun transfer4() {
+        val typeIn = "MIR"
+        val typeOut = "VISA"
+        val sumMonth = 0
+        val amount = 50000
+        val result = transfer(typeIn = typeIn, typeOut = typeOut, sumMonth = sumMonth, amount = amount)
+        assertEquals(375, result)
+    }
+    @Test
+    fun transfer5() {
+        val typeIn = "MAESTRO"
+        val typeOut = "VISA"
+        val sumMonth = 0
+        val amount = 50000
+        val result = transfer(typeIn = typeIn, typeOut = typeOut, sumMonth = sumMonth, amount = amount)
+        assertEquals(0, result)
+    }
+    @Test
+    fun transfer6() {
+        val typeIn = "MASTERCARD"
+        val typeOut = "VISA"
+        val sumMonth = 0
+        val amount = 50000
+        val result = transfer(typeIn = typeIn, typeOut = typeOut, sumMonth = sumMonth, amount = amount)
+        assertEquals(0, result)
+    }
+    @Test
+    fun transfer7() {
+        val typeIn = "sdfd"
+        val typeOut = "VISA"
+        val sumMonth = 0
+        val amount = 50000
+        val result = transfer(typeIn = typeIn, typeOut = typeOut, sumMonth = sumMonth, amount = amount)
+        assertEquals(-3, result)
+    }
+    @Test
+    fun transfer8() {
+        val typeIn = "MASTERCARD"
+        val typeOut = "VISA"
+        val sumMonth = 0
+        val amount = 200_000
+        val result = transfer(typeIn = typeIn, typeOut = typeOut, sumMonth = sumMonth, amount = amount)
+        assertEquals(1220, result)
+    }
+    @Test
+    fun transfer9() {
+        val typeIn = "MASTERCARD"
+        val typeOut = "VISA"
+        val sumMonth = 800_000
+        val amount = 50_000
+        val result = transfer(typeIn = typeIn, typeOut = typeOut, sumMonth = sumMonth, amount = amount)
+        assertEquals(0, result)
+    }
+}
